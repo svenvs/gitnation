abf799f53c6912cb33ee6a78c4035536e6639714
sven van straalen <sven.van.straalen@capgemini.com>
2022-04-26 13:00:45 +0200

    updated db function

--- api/helloWorld/index.js
+++ api/helloWorld/index.js
@@ -1,22 +1,34 @@
 const mongod = require('../mongoHandler.js');
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
-
-  const name = (req.query.name || (req.body && req.body.name));
-  const responseMessage = name
-      ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-      : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  if (name) {
-      context.bindings.outputDocument = JSON.stringify({
-          // create a random ID
-          id: new Date().toISOString() + Math.random().toString().substr(2,8),
-          name: name
-      });
-  }
-
+  await mongod.connect(context.log);
   context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: {message: responseMessage}
-  };
+    header: {
+        "Content-Type": "application/json"
+      }
+  }
+  // Read the uploaded task
+  //const newContester = req.body;
+  // Save to database
+//   const contest = await mongod.create({
+//     fullName: 'sven van Straalen',
+//     email: 'la@la.com',
+//     phone: '064634553',
+//     country: 'NL',
+//     profession: 'crazy',
+//     recruitment: true,
+// });
+
+const contest = await mongod.create({
+  fullName: req.body.fullName,
+  email: req.body.email,
+  phone: req.body.phone,
+  country: req.body.country,
+  profession: req.body.profession,
+  recruitment: req.body.recruitment,
+});
+  // Set the HTTP status to created
+  context.res.status = 201;
+  // return new object
+  context.res.body = contest;
 }
