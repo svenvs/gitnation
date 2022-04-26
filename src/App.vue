604b5c75867be7141022f0a71ec528ea1b3e6143
michellecap <michelle.ermen@capgemini.com>
2022-04-26 16:08:44 +0200

    onsubmit form call api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="background-image">
+    <form @submit="submitForm">
+      <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
 
-        <form>
-    <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-  </form>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
   </div>
 </template>
 <script>
@@ -33,8 +31,11 @@ export default {
       message: ""
     };
   },
-  async mounted() {
-    //does the post
+  methods: {
+    async submitForm(e) {
+      e.preventDefault();
+      console.log('yes hello');
+          //does the post
     const { message } = await (await fetch(
       '/api/contestor', 
       {
@@ -53,6 +54,7 @@ export default {
       })).json();
       //when done post the message
     this.message = message;
+    }
   }
 };
 </script>
