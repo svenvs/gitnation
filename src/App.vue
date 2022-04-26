0895b9af774b8379e91884051bdc020cecbc3dd9
michellecap <michelle.ermen@capgemini.com>
2022-04-26 13:47:08 +0200

    added sass and background image

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-      <h1> Hello World </h1>
+  <div class="background-image">
       {{ message }}
   </div>
 </template>
+
+<style lang="scss">
+
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.background-image {
+  background-image: url("./assets/landing.jpg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
 
 <script>
 export default {
@@ -13,9 +28,9 @@ export default {
       message: ""
     };
   },
-  async mounted() {
-    const { message } = await (await fetch("/api/message?name=hello")).json();
-    this.message = message;
-  }
+  // async mounted() {
+  //   const { message } = await (await fetch("/api/message?name=hello")).json();
+  //   this.message = message;
+  // }
 };
 </script>
