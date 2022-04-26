150fc8ad4b5de7043b8523f66357ac5cda204ff5
michellecap <michelle.ermen@capgemini.com>
2022-04-26 14:51:03 +0200

    added form

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="background-image">
-      {{ message }}
+
+
+        <form>
+    <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+  </form>
   </div>
 </template>
-
-<style lang="scss">
-
-body, html {
-  height: 100%;
-  margin: 0;
-}
-
-.background-image {
-  background-image: url("./assets/landing.jpg");
-  height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>
-
 <script>
 export default {
   name: "App",
